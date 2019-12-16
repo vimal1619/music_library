ac03a1f037f48a6f0729bc24872deda540c5c282
Vimalkumar <vimal.g.16@gmail.com>
2019-12-17 00:06:36 +0530

    [NEW] - Initial music library module and albums component

---
+++ src/app/music-library/music-library.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlbumsComponent } from './albums/albums.component';
+
+
+
+@NgModule({
+  declarations: [AlbumsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MusicLibraryModule { }
