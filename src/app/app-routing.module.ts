4ff80a6034d1cdf7449aa58f2ccdf9974b6955a5
Vimalkumar <vimal.g.16@gmail.com>
2019-12-17 07:52:38 +0530

    [MODIFY] - routes for all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { paths } from './app-paths';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PathResolveService } from './path-resolve.service';
+import { AlbumsComponent } from './music-library/albums/albums.component';
+import { EditorialComponent } from './admin/editorial/editorial.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: paths.home
+  },
+  {
+    path: paths.home,
+    component: HomeComponent
+  },
+  {
+    path: paths.admin,
+    component: EditorialComponent
+  },
+  {
+    path: paths.albums,
+    component: AlbumsComponent
+  },
+  {
+    path: '**',
+    resolve: {
+      path: PathResolveService
+    },
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
